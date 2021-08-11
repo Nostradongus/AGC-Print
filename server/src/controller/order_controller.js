d65e3eabe6c84f0ed9a0f2eb27354b2484579956
Joshue Jadie <joshue_jadie@dlsu.edu.ph>
2021-08-11 21:35:09 +0800

    Updated order functionality, added new model OrderSet for set of orders

--- server/src/controller/order_controller.js
+++ server/src/controller/order_controller.js
@@ -12,11 +12,11 @@ import uniqid from 'uniqid';
 
 // order controller object for order controller methods
 const orderController = {
-  // order controller method to retrieve and return all orders from the database
-  getAllOrders: async (req, res) => {
+  // order controller method to retrieve and return all order sets from the database
+  getAllOrderSets: async (req, res) => {
     try {
       // retrieve all orders from the database
-      const orders = await OrderService.getAllOrders();
+      const orders = await OrderService.getAllOrderSets();
       // send the array of orders back to the client
       return res.status(200).json(orders);
     } catch (err) {
@@ -25,10 +25,10 @@ const orderController = {
     }
   },
 
-  // order controller method to retrieve and return all orders of a user from the database
-  getUserOrders: async (req, res) => {
+  // order controller method to retrieve and return all order sets of a user from the database
+  getUserOrderSets: async (req, res) => {
     try {
-      const orders = await OrderService.getUserOrders(req.params.username);
+      const orders = await OrderService.getUserOrderSets(req.params.username);
 
       return res.status(200).json(orders);
     } catch (err) {
@@ -36,21 +36,10 @@ const orderController = {
     }
   },
 
-  // order controller method to retrieve and return all past orders of a user from the database
-  getUserPastOrders: async (req, res) => {
+  // order controller method to retrieve and return all past order sets of a user from the database
+  getUserPastOrderSets: async (req, res) => {
     try {
-      const orders = await OrderService.getUserPastOrders(req.params.username);
-
-      return res.status(200).json(orders);
-    } catch (err) {
-      res.status(500).json({ message: 'Server Error' });
-    }
-  },
-
-  // order controller method to retrieve and return all past orders of a user from the database
-  getUserCurrentOrders: async (req, res) => {
-    try {
-      const orders = await OrderService.getUserCurrentOrders(
+      const orders = await OrderService.getUserPastOrderSets(
         req.params.username
       );
 
@@ -60,10 +49,23 @@ const orderController = {
     }
   },
 
-  // order controller method to retrieve and return all active orders of users from the database
-  getAllActiveOrders: async (req, res) => {
+  // order controller method to retrieve and return all current order sets of a user from the database
+  getUserCurrentOrderSets: async (req, res) => {
     try {
-      const orders = await OrderService.getAllActiveOrders(req.params.username);
+      const orders = await OrderService.getUserCurrentOrderSets(
+        req.params.username
+      );
+
+      return res.status(200).json(orders);
+    } catch (err) {
+      res.status(500).json({ message: 'Server Error' });
+    }
+  },
+
+  // order controller method to retrieve and return all active order sets of a user from the database
+  getAllActiveOrderSets: async (req, res) => {
+    try {
+      const orders = await OrderService.getAllActiveOrderSets();
 
       return res.status(200).json(orders);
     } catch (err) {
@@ -88,14 +90,48 @@ const orderController = {
     }
   },
 
-  // order controller method to add new orders to the database
-  addOrders: async (req, res) => {
+  // order controller method to retrieve and return a specific order set from the database
+  getOrderSet: async (req, res) => {
     try {
+      // retrieve a specific order set from the database given the id data from the request
+      const orderSet = await OrderService.getOrderSet({ id: req.params.id });
+      // if order set exists in the database, send the data back to the client
+      if (orderSet != null) {
+        return res.status(200).json(orderSet);
+      }
+      // if order does not exist, send error status and message
+      return res.status(404).json({ message: 'Order set not found' });
+    } catch (err) {
+      // if error has occurred, send server error status and message
+      res.status(500).json({ message: 'Server Error' });
+    }
+  },
+
+  // order controller method to add new order set to the database
+  addOrderSet: async (req, res) => {
+    try {
+      // get today's date
+      const date = new Date();
+      const year = date.getFullYear().toString();
+      const month = (date.getMonth() + 1).toString().padStart(2, 0);
+      const day = date.getDate().toString().padStart(2, 0);
+      const formattedDate = `${year}-${month}-${day}`;
+
+      // generate new id for order set
+      const uniqueId = uniqid();
+
+      // add generated unique id and date to order set
+      const orderSet = {
+        id: uniqueId,
+        dateRequested: formattedDate,
+        ...req.body,
+      };
+
       // add new order/s from the cart to the database
-      const newOrders = await OrderService.addOrders(req.body);
+      const newOrderSet = await OrderService.addOrderSet(orderSet);
 
       // send order data back to the client to indicate success
-      return res.status(200).json(newOrders);
+      return res.status(200).json(newOrderSet);
     } catch (err) {
       // if error has occurred, send server error status and message
       res.status(500).json({ message: 'Server Error' });
@@ -104,13 +140,6 @@ const orderController = {
 
   // order controller method to upload and manage temporary order image coming from the client user order cart
   addToCart: (req, res) => {
-    // get today's date
-    const date = new Date();
-    const year = date.getFullYear().toString();
-    const month = (date.getMonth() + 1).toString().padStart(2, 0);
-    const day = date.getDate().toString().padStart(2, 0);
-    const formattedDate = `${year}-${month}-${day}`;
-
     // generated unique order id
     const uniqueId = uniqid();
 
@@ -125,18 +154,10 @@ const orderController = {
     const newOrder = new Order({
       id: uniqueId,
       user: req.user.username,
-      name: null,
-      email: null,
-      address: null,
-      contactNo: null,
-      payMethod: null, // TODO: temporary set to null and false required
-      workerInCharge: null,
       type: JSON.parse(JSON.stringify(req.body.type)),
       quantity: JSON.parse(JSON.stringify(req.body.quantity)),
       img: filename,
       imgPath: newFilePath,
-      price: null, // temporarily zero, will be changed by the printing company's employee
-      status: 'Processing', // always at processing status at the creation of the order
       width: JSON.parse(JSON.stringify(req.body.width)),
       height: JSON.parse(JSON.stringify(req.body.height)),
       frameOption: null,
@@ -145,8 +166,6 @@ const orderController = {
       eyelets: null,
       diecut: null,
       remarks: null,
-      dateRequested: formattedDate,
-      dateShipped: null,
     });
 
     // FOR CANVAS PRINT
@@ -198,6 +217,38 @@ const orderController = {
       return res.status(202).json({ message: 'Successfully deleted' });
     });
   },
+
+  // order controller method to delete an order from the database
+  deleteOrder: async (req, res) => {
+    try {
+      // delete an order from the database
+      const result = await OrderService.deleteOrder({ id: req.params.id });
+
+      // send result back to the client to indicate success
+      return res.status(200).json(result);
+    } catch (err) {
+      // if error has occurred, send server error status and message
+      res.status(500).json({ message: 'Server Error' });
+    }
+  },
+
+  // order controller method to delete an order set from the database
+  deleteOrderSet: async (req, res) => {
+    try {
+      // delete the orders inside the order set
+      await OrderService.deleteOrders({ orderSetId: req.params.id });
+      // delete the order set from the database
+      const result = await OrderService.deleteOrderSet({ id: req.params.id });
+
+      // send order data back to the client to indicate success
+      return res.status(200).json(result);
+    } catch (err) {
+      // if error has occurred, send server error status and message
+      res.status(500).json({ message: 'Server Error' });
+    }
+  },
+
+  // TODO: create order controller methods for updating
 };
 
 // export order controller object for routing
---
+++ server/src/model/OrderSet.js
@@ -0,0 +1,79 @@
+// import mongoose module for schema modeling
+import mongoose from 'mongoose';
+
+// order set schema for a set of orders in the application
+const OrderSetSchema = new mongoose.Schema({
+  // unique id of order set
+  id: {
+    type: String,
+    required: true,
+  },
+  // user who made the order/s
+  user: {
+    type: String,
+    required: true,
+  },
+  // the set of orders (Order objects)
+  orders: {
+    type: Array,
+    required: true,
+  },
+  // name of customer
+  name: {
+    type: String,
+    required: false,
+  },
+  // email of customer
+  email: {
+    type: String,
+    required: false,
+  },
+  // address of customer
+  address: {
+    type: String,
+    required: false,
+  },
+  // contact number of customer
+  contactNo: {
+    type: String,
+    required: false,
+  },
+  // payment method chosen by the customer
+  payMethod: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  // worker / staff in charged of the order set
+  workerInCharge: {
+    type: String,
+    required: false,
+    default: null,
+  },
+  // price of the whole order set
+  price: {
+    type: Number,
+    default: -1,
+  },
+  // current status of the order set
+  status: {
+    type: String,
+    default: 'Pending',
+  },
+  // when the order set was requested
+  dateRequested: {
+    type: String,
+    required: true,
+  },
+  // when the order was shipped
+  dateShipped: {
+    type: String,
+    default: null,
+  },
+});
+
+// implement order set schema as order set model
+const OrderSetModel = mongoose.model('OrderSet', OrderSetSchema);
+
+// export order set model for data creation and manipulation
+export default OrderSetModel;
