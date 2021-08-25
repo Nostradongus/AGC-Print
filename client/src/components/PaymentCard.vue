f63950346b9e22f473ab376ade56f7efb34e4e02
Joshue Jadie <joshue_jadie@dlsu.edu.ph>
2021-08-25 20:19:35 +0800

    added payment card component

---
+++ client/src/components/PaymentCard.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="bg-light-blue rounded-xl p-4 mx-auto mb-2 h-30 payment-card">
+        <div class="flex justify-center">
+            <div class="flex-auto">
+                <p class="text-lg manrope-regular">{{ payment.dateUploaded }}</p>
+            </div>
+            <div v-if="payment.confirmed" class="flex-auto">
+                <p class="text-lg manrope-regular">OR#{{ payment.id }}</p>
+            </div>
+            <div v-if="payment.confirmed" class="flex-auto">
+                <p class="text-lg manrope-regular">{{ payment.paymentAcc }}</p>
+            </div>
+            <div v-if="payment.confirmed" class="flex-auto">
+                <p class="text-lg manrope-regular">₱ {{ payment.amount }}</p>
+            </div>
+            <div v-if="!payment.confirmed" class="flex-auto">
+                <p class="text-lg manrope-regular">Payment Pending Verification</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PaymentCard',
+    props: {
+        payment: {
+            type: Object,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style>
+.payment-card {
+    transition: 1500ms;
+}
+
+.payment-card:hover {
+    transition: 1500ms;
+    box-shadow: 5px 1px 11px 0px rgba(0, 0, 0, 0.26);
+}
+</style>
